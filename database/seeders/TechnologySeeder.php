912ba16f32971c3162cc389908cff9f02f8bca12
FranPer23 <francescaprt@yahoo.com>
2023-06-11 16:05:02 +0200

    creazione tecnologie su db

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Technology;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +15,13 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker  $faker)
     {
-        //
+        for ($i = 0; $i < 5; $i++) {
+            $technology = new Technology();
+            $technology->name = $faker->unique()->word();
+            $technology->slug = Str::slug($technology->name, '-');
+            $technology->save();
+        }
     }
 }
